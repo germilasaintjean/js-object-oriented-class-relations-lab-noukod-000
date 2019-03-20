747202c9107a1016b30ecc37a61f84b5951b5ae4
Saint-Jean Germila <germila@noukod.com>
2019-03-20 17:30:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,19 +19,53 @@ let driver  = new Driver('Sophie')
 //
 // let store = {trips: [], passengers: []}
 //
-// let driverId = 0
-//
-// class Driver{
-//   constructor(name){
-//   this.id = ++driverId
-//   this.name = name
+let passengerId = 0
+
+class Passenger{
+  constructor(name){
+  this.id = ++passengerId
+  this.name = name
 //
 //   store.trips.push(this)
 //   store.passengers.push(this)
 //  }
 // }
 //
-// let driver = new Driver('Sophie')
+let passenger = new Passenger('Sopha')
 //
 // store.trips[]
 // store.passengers[]
+
+
+let driverId = 0
+
+class Driver{
+  constructor(name){
+  this.id = ++driverId
+  this.name = name
+
+  // store.trips.push(this)
+  // store.passengers.push(this)
+ }
+}
+
+let driver  = new Driver('Sophie')
+
+// store.trips[]
+// store.passengers[]
+//
+// let store = {trips: [], passengers: []}
+//
+let tripId = 0
+
+class Trip{
+  constructor(name){
+  this.id = ++tripId
+  this.name = name
+//
+//   store.trips.push(this)
+//   store.passengers.push(this)
+//  }
+// }
+//
+let trip = new Trip('Somalie')
